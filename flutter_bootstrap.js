2d7ae4ff0a08232d1516f7ea4bb1c93b3698eb57
roy-antor <antorroy292@gmail.com>
2025-03-22 04:29:34 +0600

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3758222217"
+    serviceWorkerVersion: "2106977783"
   }
 });
